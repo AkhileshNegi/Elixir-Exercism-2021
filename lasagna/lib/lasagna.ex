7f6dbd9d41221e45ea9d954cf12bddf2e257f9de
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-12-29 23:33:29 +0530

    submitting lasagna solution resolving feedback

--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -1,18 +1,15 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
-  def expected_minutes_in_oven(), do: 40
+  @expected_oven_time 40
 
-  # Please define the 'remaining_minutes_in_oven/1' function
-  def remaining_minutes_in_oven(remaining_min), do: 40 - remaining_min
+  def expected_minutes_in_oven(), do: @expected_oven_time
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  def remaining_minutes_in_oven(remaining_min), do: @expected_oven_time - remaining_min
+
   def preparation_time_in_minutes(layers), do: layers * 2
 
-  # Please define the 'total_time_in_minutes/2' function
   def total_time_in_minutes(layers, time_in_oven) do
     preparation_time_in_minutes(layers) + time_in_oven
   end
 
-  # Please define the 'alarm/0' function
   def alarm(), do: "Ding!"
 end
