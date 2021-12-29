de05a0e00b004bd2959fa083a56da2d85288a0ae
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-12-29 22:44:23 +0530

    submitting freelancer solution

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,20 @@
 defmodule FreelancerRates do
-  def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
-  end
+  def daily_rate(hourly_rate), do: hourly_rate * 8 / 1
 
-  def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
-  end
+  def apply_discount(before_discount, discount),
+    do: before_discount - discount * 0.01 * before_discount
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    before_discount = daily_rate(hourly_rate) * 22
+
+    apply_discount(before_discount, discount)
+    |> Float.ceil(0)
+    |> trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    total_days = budget / monthly_rate(hourly_rate, discount) * 22
+
+    Float.floor(total_days, 1)
   end
 end
