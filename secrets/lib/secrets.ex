22b29c6a6213f93ea4b534d9446eefce79107e96
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-12-29 23:12:20 +0530

    submitting secrets solution

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,33 +1,11 @@
 defmodule Secrets do
-  def secret_add(secret) do
-    function_variable = fn x ->
-      x + secret
-    end
-
-    # Please implement the secret_add/1 function
-  end
-
-  def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
-  end
-
-  def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
-  end
-
-  def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
-  end
-
-  def secret_and(secret) do
-    # Please implement the secret_and/1 function
-  end
-
-  def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
-  end
-
+  def secret_add(secret), do: &Kernel.+(&1, secret)
+  def secret_subtract(secret), do: &Kernel.-(&1, secret)
+  def secret_multiply(secret), do: &Kernel.*(&1, secret)
+  def secret_divide(secret), do: &Kernel.div(&1, secret)
+  def secret_and(secret), do: &Bitwise.band(&1, secret)
+  def secret_xor(secret), do: &Bitwise.bxor(&1, secret)
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    &(&1 |> then(secret_function1) |> then(secret_function2))
   end
 end
