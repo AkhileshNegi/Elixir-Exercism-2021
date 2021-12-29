39b759156b3f70cb2e61c576b75f8fdb223a314d
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-12-29 22:50:31 +0530

    cleanup for solution

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,19 +1,21 @@
 defmodule FreelancerRates do
-  def daily_rate(hourly_rate), do: hourly_rate * 8 / 1
+  @daily_rate 8.0
+  @monthly_billable_days 22
+
+  def daily_rate(hourly_rate), do: hourly_rate * @daily_rate
 
   def apply_discount(before_discount, discount),
     do: before_discount - discount * 0.01 * before_discount
 
   def monthly_rate(hourly_rate, discount) do
-    before_discount = daily_rate(hourly_rate) * 22
+    before_discount = daily_rate(hourly_rate) * @monthly_billable_days
 
     apply_discount(before_discount, discount)
-    |> Float.ceil(0)
-    |> trunc()
+    |> ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    total_days = budget / monthly_rate(hourly_rate, discount) * 22
+    total_days = budget / monthly_rate(hourly_rate, discount) * @monthly_billable_days
 
     Float.floor(total_days, 1)
   end
