e261a08b0846296b5134a54934033dcf8c2aa447
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-01-02 12:10:29 +0530

    submitting high-school-sweetheart solution

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,39 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    name
+    |> String.trim()
+    |> String.at(0)
+  end
+
+  def initial(name) do
+    initial = first_letter(name) |> String.upcase()
+    initial <> "."
+  end
+
+  def initials(full_name) do
+    [first_name, last_name | _] = String.split(full_name, " ")
+    initial(first_name) <> " " <> initial(last_name)
+  end
+
+  def pair(full_name1, full_name2) do
+    i1 = initials(full_name1)
+    i2 = initials(full_name2)
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{i1}  +  #{i2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
