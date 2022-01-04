df6eeb29b666e2bdc1fd3183c1d7b5da4dba37cd
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-01-04 18:25:15 +0530

    submitting high score solution with cleanups

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -5,7 +5,7 @@ defmodule HighScore do
 
   def remove_player(scores, name), do: Map.delete(scores, name)
 
-  def reset_score(scores, name), do: Map.put(scores, name, 0)
+  def reset_score(scores, name), do: add_player(scores, name, 0)
 
   def update_score(scores, name, score),
     do: Map.update(scores, name, score, &(&1 + score))
