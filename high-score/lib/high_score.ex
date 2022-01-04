1706bedf79a16dc74f50b03eb07804b2b7e69845
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-01-04 17:10:41 +0530

    submitting high score solution

---
+++ high-score/lib/high_score.ex
@@ -0,0 +1,14 @@
+defmodule HighScore do
+  def new(), do: %{}
+
+  def add_player(scores, name, score \\ 0), do: Map.put(scores, name, score)
+
+  def remove_player(scores, name), do: Map.delete(scores, name)
+
+  def reset_score(scores, name), do: Map.put(scores, name, 0)
+
+  def update_score(scores, name, score),
+    do: Map.update(scores, name, score, fn old_value -> old_value + score end)
+
+  def get_players(scores), do: Map.keys(scores)
+end
