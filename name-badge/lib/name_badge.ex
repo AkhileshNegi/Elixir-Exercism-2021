319bcb8d1539290eeef542f402aad148cc464b23
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-01-18 21:22:07 +0530

    added solution

--- name-badge/lib/name_badge.ex
+++ name-badge/lib/name_badge.ex
@@ -1,5 +1,7 @@
 defmodule NameBadge do
   def print(id, name, department) do
-    # Please implement the print/3 function
+    id = if is_nil(id), do: "", else: "[#{id}] - "
+    department = if is_nil(department), do: "OWNER", else: String.upcase(department)
+    "#{id}#{name} - #{department}"
   end
 end
